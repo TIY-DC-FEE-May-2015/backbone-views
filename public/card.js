3b86635c5e907290509c5e3c99afebe6a0ead56f
BBSR <scottamross@gmail.com>
2015-06-29 15:56:54 -0400

    basic game functionality in place

--- public/card.js
+++ public/card.js
@@ -5,14 +5,42 @@ var eventDispatcher = _.clone(Backbone.Events)
 // todo
 var Card = Backbone.View.extend({
 
+	className: "card-box",
   // more todo
   events: {
-
+  	"click .card": "flipCard",
   },
 
   // also todo
   initialize: function(viewData) {
-    
-  }
+  	this.color = viewData.color
+
+  	this.render(viewData)
+  },
+
+  render: function(viewData) {
+  	this.$el.html( this.template(viewData) )
+  },
+
+  flipCard: function() {
+  	var $card = $(this.el).find(".card")
+  	$card.css("background", this.color)
+  	//this = a backbone view
+  	//$card = a div - the el of this
+
+  	//let listener know that a card has been flipped
+  	eventDispatcher.trigger("card:flipped", this)
+  },
+
+  flipBack: function() {
+  	var $card = $(this.el).find(".card")
+  	$card.css("background", "navy")  	
+  },
+
+  removeCard: function(){
+  	$(this.el).empty()
+  },
+
+  template: Handlebars.compile( $("#card-template").html())
 
 })
--- public/index.js
+++ public/index.js
@@ -1,4 +1,8 @@
 var cards
+var flipped = 0
+var card1
+var card2
+var currentCard
 
 $(document).on("ready", function(){
 
@@ -30,6 +34,46 @@ $(document).on("ready", function(){
 
     // Return it (from the map iterator)
     return card
+
+  })
+
+  eventDispatcher.on("card:flipped", function(card){
+    //reset round when a third card is revealed
+    if (flipped === 2) {
+      card1.flipBack(card1)
+      card2.flipBack(card2)
+      flipped=0
+      currentCard = ""
+      card1=""
+      card2=""
+    }  
+
+    if (currentCard !== card.cid){
+      //store current card and card colors
+      currentCard = card.cid
+      card1 = card2
+      card2 = card
+      console.log(currentCard, card1, card2)
+
+      flipped+=1
+      //if two are revealed, play round
+      if (flipped === 2) {
+        //check for a matched pair
+        if (card1.color === card2.color) {
+          console.log("matched pair")
+          
+          //remove the cards and add points
+          $("#removed-box").append(card2.color + "<br>")
+          card1.removeCard()
+          card2.removeCard()
+          
+
+        }
+
+        //end of round; reset the storage vars
+
+      }
+    }
 
   })
 
