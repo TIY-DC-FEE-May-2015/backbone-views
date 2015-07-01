880da1858445d948f79f4bcaa9df7dc1d59b1e69
Tajaa Long <tajaalong@Tajaas-MacBook-Pro.local>
2015-06-30 21:27:34 -0400

    it's better... at least it looks pretty

--- public/card.js
+++ public/card.js
@@ -11,7 +11,7 @@ var Card = Backbone.View.extend({
         "click": "flipped",
   },
 
-  className: "name",
+  className: "back",
 
   // also todo
   initialize: function(viewData) {
@@ -24,32 +24,38 @@ var Card = Backbone.View.extend({
  flipped: function() {
   
   count ++;
+
+  this.$el.removeClass("back")
+  
   this.$el.css('background-color', this.data.color);
 
   if(count === 1){
     card1 = this.$el
-    
     color1 = card1.css("background-color")
-    //card1.click(false)
+  
   }
   if (count === 2 ){
     twocard = this.$el
     color2 = twocard.css("background-color")
-    //card2.click(false)
-      if ( color1 == color2 ){
+  
+      if ( color1 === color2 ){
         alert("You have a Match!")
         color1 = card1.css("display", "none")
         color2 = twocard.css("display", "none")
       }else {
         alert("No Match. Try Again!")
-        color1 = card1.css("background-color", "white")
-        color2 = twocard.css("background-color", "white")
+        color1 = card1.addClass("back")
+        color2 = twocard.addClass("back")
     }
   
   } 
   if(count === 3){
-    count = 0
-    count += 1
+   
+    count = 1, 
+    card1 = this.$el,
+    color1 = card1.css("background-color"), 
+    twocard = "",
+    color2 = ""
 
   }
 
